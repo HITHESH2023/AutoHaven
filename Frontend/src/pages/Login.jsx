34c03971271dda01872b7688b274aa63b2646296
HITHESH2023 <149927521+HITHESH2023@users.noreply.github.com>
2025-09-02 21:00:36 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch('https://autohaven-y8q5.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
