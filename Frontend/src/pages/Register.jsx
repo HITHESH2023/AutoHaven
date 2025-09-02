e024cfaf9df6e5da51fc20653c403a3ca6aacc6e
HITHESH2023 <149927521+HITHESH2023@users.noreply.github.com>
2025-09-02 21:00:19 +0530

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -8,7 +8,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/register', {
+      const response = await fetch('https://autohaven-y8q5.onrender.com/api/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
