026ee7e68771d0127fb60a230ac9a299d4f375fb
HITHESH2023 <149927521+HITHESH2023@users.noreply.github.com>
2025-10-15 10:55:28 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://autohaven-1.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
